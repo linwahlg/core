a6a1490b968ddf1cb1cefdb4b9679b5ba7e6602e
LudvigLemner <lemnerl@student.chalmers.se>
2023-12-07 09:25:05 +0000

    Fixed media_source.py according to pre-commit

--- homeassistant/components/sveriges_radio/media_source.py
+++ homeassistant/components/sveriges_radio/media_source.py
@@ -1,3 +1,4 @@
+"""Expose Sveriges Radio as a Media Source."""
 from __future__ import annotations
 
 from homeassistant.components.media_player import BrowseError, MediaClass, MediaType
@@ -15,37 +16,39 @@ from .const import DOMAIN, ERROR_MESSAGE_NOT_INITIALIZED, FOLDERNAME
 from .sveriges_radio import Channel, SverigesRadio
 
 
-async def async_get_media_source(hass: HomeAssistant) -> RadioMediaSource:
-    return RadioMediaSource(hass, hass.config_entries.async_entries(DOMAIN)[0])
+async def async_get_media_source(hass: HomeAssistant) -> SverigesRadioMediaSource:
+    """Set up the Media Source."""
+    return SverigesRadioMediaSource(hass, hass.config_entries.async_entries(DOMAIN)[0])
 
 
-class RadioMediaSource(MediaSource):
+class SverigesRadioMediaSource(MediaSource):
+    """Class for representing the Media Source."""
+
     name = "Sveriges Radio"
 
     def __init__(self, hass: HomeAssistant, entry: ConfigEntry) -> None:
-        """Initializes the RadioMediaSource class"""
+        """Initialize the RadioMediaSource class."""
         super().__init__(DOMAIN)
         self.hass = hass
         self.entry = entry
 
     @property
     def radio(self) -> SverigesRadio | None:
-        """Retrieves the Sveriges Radio instance from Home Assistant's data"""
+        """Retrieve the Sveriges Radio instance from Home Assistant's data."""
         return self.hass.data.get(DOMAIN)
 
     async def async_resolve_media(self, item: MediaSourceItem) -> PlayMedia:
-        """Resolves a selected Radio station to a streaming URL and returns a PlayMedia object"""
+        """Resolve a selected Radio station to a streaming URL and returns a PlayMedia object."""
         radio = self.radio or _raise_unresolvable(ERROR_MESSAGE_NOT_INITIALIZED)
 
-        station = await radio.resolve_station(station_id=item.identifier) or \
-            _raise_unresolvable("Radio station is no longer available")
+        station = await radio.resolve_station(
+            station_id=item.identifier
+        ) or _raise_unresolvable("Radio station is no longer available")
 
         return PlayMedia(station.url, "audio/mpeg")
 
     async def async_browse_media(self, item: MediaSourceItem) -> BrowseMediaSource:
-        """Returns media items for browsing, structured as BrowseMediaSource objects"""
-        radio = self.radio or _raise_browse_error()
-
+        """Return media items for browsing, structured as BrowseMediaSource objects."""
         category, _, program_info = (item.identifier or "").partition("/")
         title = _determine_title(category, program_info)
 
@@ -61,15 +64,19 @@ class RadioMediaSource(MediaSource):
             children=await self._build_media_children(item, category),
         )
 
-    async def _build_media_children(self, item: MediaSourceItem, category: str) -> list[BrowseMediaSource]:
-        """Builds and returns a list of BrowseMediaSource objects as children media items based on the category specified in the MediaSourceItem."""
+    async def _build_media_children(
+        self, item: MediaSourceItem, category: str
+    ) -> list[BrowseMediaSource]:
+        """Build and return a list of BrowseMediaSource objects as children media items based on the category specified in the MediaSourceItem."""
         if category == FOLDERNAME:
             return await self._async_build_programs(item)
         return await self._async_build_channels(item)
 
     @callback
-    async def _async_build_channels(self, item: MediaSourceItem) -> list[BrowseMediaSource]:
-        """Builds a list of channel BrowseMediaSource objects for the given media item"""
+    async def _async_build_channels(
+        self, item: MediaSourceItem
+    ) -> list[BrowseMediaSource]:
+        """Build a list of channel BrowseMediaSource objects for the given media item."""
         category, _, _ = (item.identifier or "").partition("/")
         if category:
             return []
@@ -83,7 +90,9 @@ class RadioMediaSource(MediaSource):
         ]
 
     @callback
-    async def _async_build_programs(self, item: MediaSourceItem) -> list[BrowseMediaSource]:
+    async def _async_build_programs(
+        self, item: MediaSourceItem
+    ) -> list[BrowseMediaSource]:
         radio = self.radio or _raise_browse_error()
         category, _, program_code = (item.identifier or "").partition("/")
 
@@ -96,7 +105,9 @@ class RadioMediaSource(MediaSource):
 
         programs = await radio.programs(programs_list=[])
         return [
-            _create_browse_media_source(program, MediaType.MUSIC, False, f"{FOLDERNAME}/{program.station_id}")
+            _create_browse_media_source(
+                program, MediaType.MUSIC, False, f"{FOLDERNAME}/{program.station_id}"
+            )
             for program in programs
         ]
 
@@ -125,15 +136,17 @@ def _create_browse_media_source(entity, media_content_type, can_play, identifier
 
 
 def _raise_unresolvable(error_message: str):
-    """Raise an Unresolvable error with the given message"""
+    """Raise an Unresolvable error with the given message."""
     raise Unresolvable(error_message)
 
+
 def _raise_browse_error():
-    """Raise a BrowseError when the radio is not initialized"""
+    """Raise a BrowseError when the radio is not initialized."""
     raise BrowseError(ERROR_MESSAGE_NOT_INITIALIZED)
 
+
 def _create_podcast_root() -> BrowseMediaSource:
-    """Create a root BrowseMediaSource for podcasts"""
+    """Create a root BrowseMediaSource for podcasts."""
     return BrowseMediaSource(
         domain=DOMAIN,
         identifier=FOLDERNAME,
@@ -145,11 +158,11 @@ def _create_podcast_root() -> BrowseMediaSource:
         thumbnail="https://www.pngarts.com/files/7/Podcast-Symbol-Transparent-Background-PNG.png",
     )
 
+
 def _determine_title(category: str, program_info: str) -> str:
-    """Determine the title for the media based on category and program information"""
+    """Determine the title for the media based on category and program information."""
     if category == FOLDERNAME and program_info:
         return program_info  # Or a more appropriate title based on program_info
     if category == FOLDERNAME:
         return FOLDERNAME
     return "Sveriges Radio"
-
